a30c38fe794211e29cf307923d8d12575b319351
sakthinathan <150451065+sakthiwolf@users.noreply.github.com>
2026-04-07 20:18:28 +0530

    api

--- src/Config/Api.ts
+++ src/Config/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// export const API_URL = "http://localhost:8080";
+export const API_URL = "https://seenubackend.onrender.com";
 export const DEPLOYED_URL = "https://seenubackend.onrender.com"
 // change api
 
